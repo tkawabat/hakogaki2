bdc2a8d8a1d51a7d68d4bfb1e35673fadc96e7fd
tkawabat <tosiki.kawabata@gmail.com>
2025-01-24 00:46:48 +0900

    ヘッダー追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+'use client'
+
 import { BoxWritingEditor } from '../components/BoxWritingEditor'
 import { ChatUI } from '../components/ChatUI'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
     <div className="min-h-screen p-8">
+      <Header />
       <BoxWritingEditor />
       <ChatUI />
     </div>
