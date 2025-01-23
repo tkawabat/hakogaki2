16a403da6555cdb3f5d94cd129ba2c5aa1ff005d
tkawabat <tosiki.kawabata@gmail.com>
2025-01-23 23:42:39 +0900

    ボックスの削除ボタンを右上に移動

--- src/components/BoxWritingEditor.tsx
+++ src/components/BoxWritingEditor.tsx
@@ -32,9 +32,11 @@ export function BoxWritingEditor() {
             />
             <button
               onClick={() => handleDeleteParagraph(index)}
-              className="absolute -left-8 top-2 opacity-0 group-hover:opacity-100 text-red-500 hover:text-red-700"
+              className="absolute right-0 top-0 opacity-0 group-hover:opacity-100 text-red-500 hover:text-red-700 p-1"
             >
-              ×
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+              </svg>
             </button>
           </div>
         ))}
