2cf72e34775775269e5436f02f6a8e650621b20f
tkawabat <tosiki.kawabata@gmail.com>
2025-06-24 21:53:00 +0900

    以前作ったものを掃除。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 'use client'
 
-import { BoxWritingEditor } from '../components/BoxWritingEditor'
-import { ChatUI } from '../components/ChatUI'
-import Header from '../components/Header'
-
 export default function Home() {
   return (
     <div className="min-h-screen p-8">
-      <Header />
-      <BoxWritingEditor />
-      <ChatUI />
     </div>
   )
 }
