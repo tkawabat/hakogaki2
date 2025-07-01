243f2a03f2621aa5926e9ff187cc405f1932318b
tkawabat <tosiki.kawabata@gmail.com>
2025-07-01 21:38:40 +0900

    feat: トップページにテストボタンを配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <div className="min-h-screen p-8">
-      aaaa
+      <Button>Click me</Button>
     </div>
   )
 }
